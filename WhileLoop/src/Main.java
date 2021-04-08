6b7fdba4147c67d9ffaa84d8618563f353425f63
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-08 22:42:07 +0100

    issue done. closes #4.

---
+++ WhileLoop/src/Main.java
@@ -0,0 +1,48 @@
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenNumbersFound = 0;
+
+        while (number <= finishNumber) {
+            number ++;
+            if(!isEvenNumber(number)) {
+                continue;
+            }
+            evenNumbersFound++;
+
+            if(evenNumbersFound >=5) {
+                break;
+            }
+            System.out.println("Even Number " + number);
+        }
+
+        int count = 6;
+
+        do {
+            System.out.println("Count value was " + count);
+            count++;
+        } while (count !=6);
+
+    }
+
+    // Create a method called isEvenNumber that takes a parameter of type int
+    // Its purpose is to determine if the argument passed the the method is
+    // an even number or not.
+    // return true if an even number, otherwise return false;
+
+    public static boolean isEvenNumber(int number){
+        if((number % 2) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
